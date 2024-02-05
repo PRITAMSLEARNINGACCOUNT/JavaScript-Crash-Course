321cc6484e4f2710209f62e36b28f4e211ce8e4e
pritamstech <pritamstech@hotmail.com>
2024-02-05 13:54:02 +0530

    Changes Added To Regular Expression

--- Regular_Expressions.js
+++ Regular_Expressions.js
@@ -6,6 +6,9 @@ let Pattern = /pritam/gi;
 let Name = "Pritam Is Not Pritam He Is Pritam";
 console.log(Name.replace(Pattern, "Fucker"));
 console.log("So I Have Created A Pattern And Use 'replace' Method Which Is Oviously A String Method To Replace All Of The Occurances Of That Pattern With The Second Argument Provided By Me.");
+console.log("This Program Contains One More Example Of Regular Expressions Commented Out Below.If Anybody Wants To Cheek That Example Out Can Cheeck Out That Example Too.");
+console.log("Lastly I Am Adding A Website Below Which Is Outstandingly Helpful For Learning Regular Expressions.");
+console.log("https://regexr.com")
 // const regex = /(Harry){2}/gi
 // const text = "Harryharry is a very very nice awesome nice very boy"
 // console.log(text.replace(regex, "VERY"))

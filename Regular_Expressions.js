53c14bf895622a57ce080315fa2873554859f206
pritamstech <pritamstech@hotmail.com>
2024-02-05 13:50:45 +0530

    Regular Expressions Added

---
+++ Regular_Expressions.js
@@ -0,0 +1,12 @@
+console.log('This Program Is About Regular Expressions In JavaScript(JS)');
+console.log("Regular Expression Is All About Creating A Pattern Which Finds Whatever Pattern I Wanted From Whereever I Want.");
+console.log("After Finding The Pattern Of Our Own Choice We Can Use Several Methods Which Accepts Regular Expression & Then Do Whatever That Function Is Supposed To Do.");
+
+let Pattern = /pritam/gi;
+let Name = "Pritam Is Not Pritam He Is Pritam";
+console.log(Name.replace(Pattern, "Fucker"));
+console.log("So I Have Created A Pattern And Use 'replace' Method Which Is Oviously A String Method To Replace All Of The Occurances Of That Pattern With The Second Argument Provided By Me.");
+// const regex = /(Harry){2}/gi
+// const text = "Harryharry is a very very nice awesome nice very boy"
+// console.log(text.replace(regex, "VERY"))
+// console.log(regex);

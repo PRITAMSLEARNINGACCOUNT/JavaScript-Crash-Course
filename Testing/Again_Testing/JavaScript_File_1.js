7095b4000d176897a93c2d703553ef94855554c5
pritamstech <pritamstech@hotmail.com>
2024-03-27 10:50:43 +0530

    Testing Modified

--- Testing/Again_Testing/JavaScript_File_1.js
+++ Testing/Again_Testing/JavaScript_File_1.js
@@ -1,13 +1,12 @@
 // Common JS
-async function Testing_Func(params) {
-  let Fetch_News = async () => {
-    let Response = await fetch(
-      "https://newsapi.org/v2/top-headlines?country=us&apiKey=cf7fec402fd1484c9d2561c5a2ac1b94"
-    );
-    let JSON = await Response.json();
-    return JSON.articles;
-  };
-}
+
+let Fetch_News = async () => {
+  let Response = await fetch(
+    "https://newsapi.org/v2/top-headlines?country=us&apiKey=cf7fec402fd1484c9d2561c5a2ac1b94"
+  );
+  let JSON = await Response.json();
+  return JSON.articles;
+};
 
 let Fetched_Newses = await Fetch_News();
-module.exports = Fetched_Newses;
+// module.exports = Fetched_Newses;
